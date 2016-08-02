f6782e2d806a14b3b6605e3a1015a9d2e71a9127
Susan Dreher <sdreher@columbia.edu>
2016-08-02 11:54:46 -0400

    npm3: remove relative references to node_modules

--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ require('!file?name=[name].[ext]!../static/index.html');
 require('./static.js');
 
 // load and apply css
-require('../node_modules/bootstrap/dist/css/bootstrap.min.css');
-require('../node_modules/bootstrap-arrow-buttons/dist/' +
-        'css/bootstrap-arrow-buttons.css');
+require('!style!css!bootstrap/dist/css/bootstrap.min.css');
 require('../static/css/common.css');
 require('../static/css/imagemapchart.css');
 
 var jQuery = require('jquery');
-var module = require('../node_modules/imagemapchart/src/index.js');
+var module = require('imagemapchart/src/index.js');
 
 jQuery(document).ready(function() {
     module.ImageMapChartApp.initialize({
