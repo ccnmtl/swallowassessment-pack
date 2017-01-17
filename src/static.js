bc28c24c72ebdb7837ff76488edfe42e3fbf7a29
zarina <zarina@columbia.edu>
2017-01-17 15:29:14 -0500

    Added path to new logo images, and PDF file version

--- src/static.js
+++ src/static.js
@@ -1,1 +1,3 @@
-require('!file?name=[name].[ext]!../static/img/ctl-logo.png');
+require('!file?name=[name].[ext]!../static/img/logo-ctl.png');
+require('!file?name=[name].[ext]!../static/img/logo-cdm.png');
+require('!file?name=[name].[ext]!../static/pdf/print-swallowassessment.pdf');
