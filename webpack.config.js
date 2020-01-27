3b2260bb143401b852898a5d1aa58ddde5442604
Susan Dreher <sdreher@gmail.com>
2020-01-27 14:47:04 -0500

    Fix image loading

--- webpack.config.js
+++ webpack.config.js
@@ -23,13 +23,23 @@ module.exports = {
             {
                 test: /\.(png|svg|jpg|gif)$/,
                 use: [
-                    'file-loader'
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false
+                        }
+                    }
                 ]
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
                 use: [
-                    'file-loader'
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false
+                        }
+                    }
                 ]
             }
         ]
